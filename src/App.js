d80ad0af5219319fc7b6e608f9bb85deb983937e
Poonam Patel <poonam.patel@triveniglobalsoft.com>
2022-08-24 13:30:57 +0530

    Changes App.js file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import version from '../package.json'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          welcome my world
+          welcome to Triveni
         </p>
-
+        <p>UI version :- {version.version}</p>
       </header>
     </div>
   );
